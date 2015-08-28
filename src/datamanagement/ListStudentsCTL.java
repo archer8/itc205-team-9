fc901d25382c75afa4eaab0d03ee5806b8ec9ef1
shelbimaree <shelbimaree@hotmail.com>
2015-08-28 16:48:03 +1000

    ListStudentCTL and ListUnitsCtl

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -1,8 +1,25 @@
 package datamanagement;
-        public class ListStudentsCTL {
-private StudentManager sm;
-public ListStudentsCTL() {sm = StudentManager.get();}
-            public void listStudents( IStudentLister lister, String unitCode ) {
+
+public class ListStudentsCtl {
+  private StudentManager sm;
+
+  public ListStudentsCTL() {
+    sm = StudentManager.get();
+  }
+
+  /**
+   * listStudents does something.
+   *
+   * @param lister
+   *          Param lister.
+   * @param unitCode
+   *          Param unitCode.
+   */
+  public void listStudents(IStudentLister lister, String unitCode) {
     lister.clearStudents();
-                StudentMap students = sm.getStudentsByUnit( unitCode );
-for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
+    StudentMap students = sm.getStudentsByUnit(unitCode);
+    for (Integer id : students.keySet()) {
+      lister.addStudent(students.get(id));
+    }
+  }
+}
--- src/datamanagement/ListUnitsCTL.java
+++ src/datamanagement/ListUnitsCTL.java
@@ -1,10 +1,23 @@
 package datamanagement;
-public class ListUnitsCTL {
-    private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.UM();
+
+public class ListUnitsCtl {
+  private UnitManager um;
+
+  public ListUnitsCTL() {
+    um = UnitManager.UM();
+  }
+
+  /**
+   * listUnits does something.
+   *
+   * @param lister
+   *          Param lister.
+   */
+  public void listUnits(IUnitLister lister) {
+    lister.clearUnits();
+    UnitMap units = um.getUnits();
+    for (String s : units.keySet()) {
+      lister.addUnit(units.get(s));
+    }
+  }
 }
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
