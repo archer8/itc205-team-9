4d6ae8a1991d44e27628ce6e3b3c24047b24df71
jpinaz92 <jpinaz92@gmail.com>
2015-08-28 14:47:48 +1000

    removed console output
    removed the System.out.println

--- src/datamanagement/StudentUnitRecord.java
+++ src/datamanagement/StudentUnitRecord.java
@@ -30,7 +30,7 @@ public class StudentUnitRecord implements IStudentUnitRecord {
     
     public void setAsg1(float a1) {
         boolean as1IsValid = (a1 > 0 || a1 < UnitManager.UM().getUnit(unitCode_).getAsg1Weight());
-        System.out.println(as1IsValid);
+
 	if (as1IsValid == false) {
             throw new RuntimeException("Mark cannot be less than zero or greater than assessment weight");
 	}
